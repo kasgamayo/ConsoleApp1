b565e0bfb671558ebf97606544aa648dd842a172
kasgamayo <kagamayow@gmail.com>
2026-04-14 11:24:47 +0800

    Revert "Remove comment from using directive"
    This reverts commit 6bcd638984b48aca3741565f853c08f99d82e07f.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
+using System; // Allows us to use classes from the System namespace, like Console [4]
 
 namespace BankingApp
 {
